4e4ce43e7f1215a254dd2e8f9566880e753b1705
Shogo Tabata <>
2019-02-06 23:44:17 +0900

    ignore設定 パッケージ管理

--- analyze_candle.py
+++ analyze_candle.py
@@ -1,11 +1,43 @@
 from enum import Enum
-class AnalyzeCandle:
-    #陽線か陰線か
-    class CandleType(Enum):
-        POSITIVE = 0
-        NEGATIVE = 1
-    class CandleKind(Enum):
-        BIG_WHITE = 0
+#陽線か陰線か
+class CandleDirectionType(Enum):
+    NONE = 99
+    STAY = 0
+    POSITIVE = 1
+    NEGATIVE = 2
 
-        BIG_BLACK = 100
-    def 
+#種類
+class CandleFluctuationType(Enum):
+    NONE = 99
+    BIG = 0 #
+    STRAIGHT = 1 # 坊主
+    HUMMER = 2 # トンカチ
+
+#ローソクの種類
+class CandleState:
+    def __init__(self,direction,fluctuation):
+        self.direction = direction
+        self.fluctuation = fluctuation
+    def __repr__(self):
+        return "[ candle direction '%s' : fluctuation '%s' ]" % (self.direction, self.fluctuation)
+    
+#------------------------------------------------
+#分析
+class CandleAnalyzer:
+    def __init__(self):
+        pass
+        #ローソク足分析
+    def analyze_candle(self,data):
+        direction   = self.analyze_candle_direction(data)
+        fluctuation = self.analyze_candle_fluctuation(data)
+        state       = CandleState(direction,fluctuation)
+        return state
+        # 陰陽判定
+    def analyze_candle_direction(self,data):
+        ret = CandleDirectionType.NONE
+        return ret
+        # 種類分析
+    def analyze_candle_fluctuation(self,data):
+        ret = CandleFluctuationType.NONE
+        return ret
+
--- debug.py
+++ debug.py
@@ -1,9 +1,15 @@
 import pandas_datareader.data as web
 from datetime import datetime
 import matplotlib.pyplot as plt
+from analyze_candle import CandleAnalyzer
 plt.style.use('ggplot')
 start = datetime(2000, 1, 1)
 end = datetime(2018, 9, 17)
 df = web.DataReader('2160.JP', 'stooq')
-df.plot()
-plt.show()
+data = df.iloc[0,[0,1,2,3]]
+print(data)
+ca = CandleAnalyzer()
+result = ca.analyze_candle(data)
+print(result)
+#df.plot()
+#plt.show()
